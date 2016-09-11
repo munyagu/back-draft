ca9205c51eec1a782375e6a32a6750b688efebfd
nagasawa <nagasawa@barbwire.co.jp>
2016-09-11 12:26:01 +0900

    first commit

---
+++ back-draft.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name: Backdraft
+Plugin URI: http://munyagu.com/
+Description: Set post status 'draft' when restore from trash box.
+Version: 1.0
+Author: munyagu
+Author URI: http://munyagu.com/
+License: GPL2
+*/
+
+
+function backdraft_trashed_post( $post_id ){
+    global $wpdb;
+    $result = $wpdb->update(
+        $wpdb->postmeta,
+        array(
+            'meta_value' => 'draft'
+        ),
+        array(
+            'post_id' => $post_id,
+            'meta_key' => '_wp_trash_meta_status'
+        )
+    );
+}
+add_action( 'trashed_post', 'backdraft_trashed_post' );
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Backdraft ===
+Contributors: munyagu
+Donate link: http://munyagu.com/donate/
+Tags: post
+Requires at least: 3.8
+Tested up to: 3.6.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+
+== Description ==
+
+You rescure post form trush box, this plugin makes it status 'draft'.
+And you can elaborate about it before publish.
+
+== Installation ==
+
+1. Install and activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 1.0 =
+First release.
